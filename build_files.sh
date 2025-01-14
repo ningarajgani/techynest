6d3279f0f437d21139184ddfeadfc06213547e72
ningarajgani <116344375+ningarajgani@users.noreply.github.com>
2025-01-14 15:08:32 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
-# build_files.sh
+#!/bin/bash
+
+# Install dependencies
 pip3 install -r requirements.txt
-python3 manage.py collectstatic
+
+# Run migrations
+python3 manage.py migrate
+
+# Collect static files
+python3 manage.py collectstatic --noinput
